93f4bb5e7c0b1fe88f63b945f705eacf9f55bff4
David <davidecap00@hotmail.it>
2022-02-23 00:14:16 +0100

    WIP

--- Assets/Scripts/GunCreator.cs
+++ Assets/Scripts/GunCreator.cs
@@ -10,14 +10,14 @@ public class GunCreator : MonoBehaviour
     private Sprite[] _barrels;
     private Sprite[] _grips;
     private Sprite[] _mags;
-    private Sprite[] _silencers;
+    private Sprite[] _muzzles;
 
     public SpriteRenderer body;
     public SpriteRenderer stock;
     public SpriteRenderer barrel;
     public SpriteRenderer grip;
     public SpriteRenderer mag;
-    public SpriteRenderer silencer;
+    public SpriteRenderer muzzle;
 
     // Start is called before the first frame update
     void Start()
@@ -27,22 +27,38 @@ public class GunCreator : MonoBehaviour
         _barrels = Resources.LoadAll<Sprite>("barrels");
         _grips = Resources.LoadAll<Sprite>("grips");
         _mags = Resources.LoadAll<Sprite>("mags");
-        //_silencers = Resources.LoadAll<Sprite>("silencers");
+        _muzzles = Resources.LoadAll<Sprite>("silencers");
     }
 
     // Update is called once per frame
 
     public void keyPressed(InputAction.CallbackContext context)
     {   
-        if(context.performed)
+        if(context.started)
         {
             body.sprite = _bodies[Random.Range(0,_bodies.Length)];
             stock.sprite = _stocks[Random.Range(0,_stocks.Length)];
             barrel.sprite = _barrels[Random.Range(0,_barrels.Length)];
             grip.sprite = _grips[Random.Range(0,_grips.Length)];
             mag.sprite = _mags[Random.Range(0,_mags.Length)];
-            //silencer.sprite = _silencers[Random.Range(0,_silencers.Length)];
+            muzzle.sprite = _muzzles[Random.Range(0,_muzzles.Length)];
+            updateVisual();
+
         }
+
+    }
+
+    private void updateVisual()
+    {
+        Destroy(barrel.gameObject.GetComponent<BoxCollider2D>());
+        BoxCollider2D collider = barrel.gameObject.AddComponent<BoxCollider2D>() as BoxCollider2D;
+
+        var coords = collider.size;
+        var barrelCenter = collider.bounds.center;
+        Debug.Log("barrel collider size = "+coords);
+        muzzle.transform.position = new Vector3(barrelCenter.x + coords.x / 2, 0f, 0f);
+
+
 
     }
 
