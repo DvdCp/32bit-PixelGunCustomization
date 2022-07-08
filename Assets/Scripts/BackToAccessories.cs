47155e7878d6f9d6a578ed7b6a3459746b51a8e8
DvdVp <davidecap00@hotmail.it>
2022-07-09 01:34:57 +0200

    WIP on scroll list

--- Assets/Scripts/BackToAccessories.cs
+++ Assets/Scripts/BackToAccessories.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BackToAccessories : MonoBehaviour
@@ -7,9 +5,23 @@ public class BackToAccessories : MonoBehaviour
     [SerializeField] GameObject accessoryVariantsList;
     [SerializeField] GameObject accessoriesList;
 
+    private Transform Scroll;
+    private Transform Panel;
+
+    private void Start()
+    {
+        // Getting reference to UI elements
+        Scroll = accessoryVariantsList.transform.Find("Scroll");
+        Panel = Scroll.Find("Panel");
+    }
+
     public void onClick()
     {
         accessoryVariantsList.SetActive(false);
         accessoriesList.SetActive(true);
+
+        // Removing all accessories except the first (No_Accessory button)
+        for (int i = 1; i < Panel.childCount; i++)
+            Destroy(Panel.GetChild(i).gameObject);
     }
 }
