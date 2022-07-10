34c3bc5f8792888508260323cd6a9ca9fbdb46f9
DvdVp <davidecap00@hotmail.it>
2022-07-10 20:28:07 +0200

    WIP on gun (fix on firepoint)

--- Assets/Scripts/UI/UpdateMuzzlePosition.cs
+++ Assets/Scripts/UI/UpdateMuzzlePosition.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class UpdateMuzzlePosition : MonoBehaviour
 {
     Sprite barrel;
-    GameObject firePoint;
 
     void Update()
     {
@@ -13,11 +12,6 @@ public class UpdateMuzzlePosition : MonoBehaviour
 
         var newX = barrel.bounds.center.x + barrel.bounds.size.x / 2;
         transform.localPosition = new Vector3(newX, 0f, 0f);
-
-        //// Updating firePoint position
-        var firePointX = transform.GetComponent<SpriteRenderer>().sprite.bounds.center.x;
-        var firePointY = transform.GetComponent<SpriteRenderer>().sprite.bounds.center.y;
-
-        firePoint.transform.localPosition = new Vector3(firePointX, firePointY / 2, 0f);
     }
+
 }
