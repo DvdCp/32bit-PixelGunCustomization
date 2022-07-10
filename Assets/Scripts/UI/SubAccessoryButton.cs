8dd5a521f5cdcce5591f9263fe5ba12bbb830e1f
DvdVp <davidecap00@hotmail.it>
2022-07-10 19:39:52 +0200

    WIP on UI features

--- Assets/Scripts/UI/SubAccessoryButton.cs
+++ Assets/Scripts/UI/SubAccessoryButton.cs
@@ -23,6 +23,7 @@ public class SubAccessoryButton : MonoBehaviour
     public void setVariantImage(Sprite sprite)
     {
         variantImage.sprite = sprite;
+        UpdatingRectTransform(sprite);
     }
 
     public void setAccessoryRenderer(SpriteRenderer renderer)
@@ -40,6 +41,7 @@ public class SubAccessoryButton : MonoBehaviour
 
         // update sprite in button and on weapon
         variantImage.sprite = variantList[variantIndex];
+        UpdatingRectTransform(variantList[variantIndex]);
         accessoryRenderer.sprite = variantList[variantIndex];
     }
 
@@ -54,6 +56,15 @@ public class SubAccessoryButton : MonoBehaviour
 
         // update sprite in button and on weapon
         variantImage.sprite = variantList[variantIndex];
+        UpdatingRectTransform(variantList[variantIndex]);
         accessoryRenderer.sprite = variantList[variantIndex];
+
+    }
+
+    private void UpdatingRectTransform(Sprite sprite)
+    {
+        var newWidth = sprite.rect.width * 10;
+        var newHeigth = sprite.rect.height * 10;
+        variantImage.rectTransform.sizeDelta = new Vector2(newWidth, newHeigth);
     }
 }
