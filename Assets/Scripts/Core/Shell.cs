051a009bd531770456e896592da5696fe8c9e260
DavidC <dav.capuano98@gmail.com>
2022-07-14 18:41:40 +0200

    improving shell eject

--- Assets/Scripts/Core/Shell.cs
+++ Assets/Scripts/Core/Shell.cs
@@ -2,13 +2,24 @@ using UnityEngine;
 
 public class Shell : MonoBehaviour
 {
-  
+    [SerializeField] float ejectingRotationSpeed;
+
     Rigidbody2D _rb;
 
+
     void Start()
-    {   
+    {
+        // Random ejecting trajectory (backwards)
+        float randomX = Random.Range(0.4f, 1f);
+        Vector3 ejectingTrajecotry = new Vector3(-randomX/2f, 1f, 0f);
+
         _rb = GetComponent<Rigidbody2D>();
-        _rb.AddForce(transform.up, ForceMode2D.Impulse);
+        _rb.AddForce(ejectingTrajecotry, ForceMode2D.Impulse);
+    }
+
+    private void Update()
+    {
+        transform.Rotate(0f, 0f, 360 * ejectingRotationSpeed * Time.deltaTime);
     }
 
     private void OnBecameInvisible() {
